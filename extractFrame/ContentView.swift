8276e9cf70f766d5ee7f4938cd21b53a928edefb
louis-kaiser <louis.kaiser0203@icloud.com>
2024-02-15 09:59:27 +0100

    removed cameraView

--- extractFrame/ContentView.swift
+++ extractFrame/ContentView.swift
@@ -8,45 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var showFrames = false // A state variable to control the view mode
     @ObservedObject var cameraViewModel = CameraViewModel() // An instance of the camera view model
     
     var body: some View {
         VStack {
-            if showFrames {
-                FramesView(cameraViewModel: cameraViewModel) // Show the frames view if the state is true
+            if cameraViewModel.frames.count >= 30 {
+                FramesView(cameraViewModel: cameraViewModel)
             } else {
-                CameraView(cameraViewModel: cameraViewModel) // Show the camera view if the state is false
-            }
-            Button(action: {
-                showFrames.toggle() // Toggle the state when the button is pressed
-            }) {
-                Text(showFrames ? "Show Camera" : "Show Frames") // Change the button text based on the state
+                Text("Wait until all Frames are loded")
             }
         }
-        .padding()
-    }
-}
-
-struct CameraView: View {
-    @ObservedObject var cameraViewModel: CameraViewModel // A reference to the camera view model
-    
-    var body: some View {
-        VStack {
-            if let frame = cameraViewModel.currentFrame {
-                Image(nsImage: frame)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            } else {
-                Text("Waiting for camera feed...")
-            }
-        }
+        .ignoresSafeArea(.all)
+        .background(Color.black)
         .onAppear {
             cameraViewModel.startCapture()
         }
-        .onDisappear {
-            cameraViewModel.stopCapture()
-        }
+        
     }
 }
 
@@ -54,14 +31,12 @@ struct FramesView: View {
     @ObservedObject var cameraViewModel: CameraViewModel // A reference to the camera view model
     
     var body: some View {
-        ScrollView(.horizontal) {
-            ZStack {
-                ForEach(0..<30) { index in
-                    Image(nsImage: cameraViewModel.frames[index])
-                        .resizable()
-                        .blendMode(.screen) // screen or lighten
-                        .aspectRatio(contentMode: .fit)
-                }
+        ZStack {
+            ForEach(0..<30) { index in
+                Image(nsImage: cameraViewModel.frames[index])
+                    .resizable()
+                    .blendMode(.screen) // screen or lighten
+                    .aspectRatio(contentMode: .fit)
             }
         }
     }
@@ -112,21 +87,9 @@ class CameraViewModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
         let ciImage = CIImage(cvImageBuffer: imageBuffer)
         let context = CIContext()
         
-        // Apply contrast adjustment to enhance the mask
-        let contrastFilter = CIFilter(name: "CIColorControls")!
-        contrastFilter.setValue(ciImage, forKey: kCIInputImageKey)
-        contrastFilter.setValue(2.0, forKey: kCIInputContrastKey) // Adjust contrast as needed
-        guard let contrastAdjusted = contrastFilter.outputImage else { return }
-        
-        // Apply gaussian blur for smoothing
-        let blurFilter = CIFilter(name: "CIGaussianBlur")!
-        blurFilter.setValue(contrastAdjusted, forKey: kCIInputImageKey)
-        blurFilter.setValue(5.0, forKey: kCIInputRadiusKey) // Adjust blur radius as needed
-        guard let blurredImage = blurFilter.outputImage else { return }
-        
         // Apply threshold filter
         let thresholdFilter = CIFilter(name: "CIColorThreshold")!
-        thresholdFilter.setValue(blurredImage, forKey: kCIInputImageKey)
+        thresholdFilter.setValue(ciImage, forKey: kCIInputImageKey)
         thresholdFilter.setValue(0.7, forKey: "inputThreshold")
         guard let thresholdedImage = thresholdFilter.outputImage else { return }
 
@@ -143,6 +106,4 @@ class CameraViewModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
         }
     }
 
-
 }
-
